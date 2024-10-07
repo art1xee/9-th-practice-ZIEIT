8f1ef142c80584826afb6eeb8f5bf88a4adb928f
Maksym <makczub@gmail.com>
2024-10-07 16:17:28 +0300

    first commit

---
+++ src/Home.js
@@ -0,0 +1,19 @@
+import React, { useContext } from "react";
+import { AppContext } from "./context";
+import { FaBars } from "react-icons/fa";
+
+const Home = () => {
+  const { openSideBar, openModal } = useContext(AppContext);
+  return (
+    <main>
+      <button onClick={openSideBar} className="sidebar-toggle">
+        <FaBars />
+      </button>
+      <button onClick={openModal} className="btn">
+        show modal
+      </button>
+    </main>
+  );
+};
+
+export default Home;
